96d5f34831f89473d936e83acbff24caeef489cc
Jay Denebeim <jayd@tucsonembedded.com>
2013-11-05 16:25:04 -0700

    Micropost first steps

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   has_secure_password
   before_save { self.email.downcase! }
   validates :name, presence:true, length: { maximum: 50 }
